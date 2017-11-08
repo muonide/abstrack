587176b73b4409bed7cc6afe742b7e9a7b15cf79
sllauQ <32281574+sllauQ@users.noreply.github.com>
2017-11-08 16:58:02 -0600

    Fixed for nav-bar

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,39 @@ var app = express();
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-  res.render('index', {title: 'Arkansas Balloon', home: true});
+  res.render('index', {title: 'Arkansas Balloon', home: true, page: 'home', home: 'active'});
 });
 
 router.get('/missions', function(req, res, next){
-  res.render('missions', {title: 'Missions'});
+  res.render('missions', {title: 'Missions', missions: 'active'});
 });
 
 
-router.get('/prediction', function(req, res, next){
-  res.render('prediction', {title: 'Prediction'});
+
+router.get('/predictions', function(req, res, next){
+  res.render('prediction', {title: 'Predictions', prediction: 'active'});
 });
 
 
-// router.get('/recovery', function(req, res, next){
-//   res.render('recovery');
-// });
+router.get('/recovery', function(req, res, next){
+   res.render('recovery', {title: 'Recovery', recovery: 'active'});
+});
 
 router.get('/found', function(req, res, next){
-  res.render('found');
+  res.render('found', {title: 'Found', found: 'active'});
 });
 
 
-// router.get('/about', function(req, res, next){
-//   res.render('about');
-// }); 
+router.get('/about', function(req, res, next){
+   res.render('about', {title: 'About', about: 'active'});
+}); 
 
 
 
 // must be at the bottom, will take precedence over other views
+
 router.get('*', function(req, res){
+  console.log('Error 404');
   res.render('error', {title: 'Error 404'});
 });
 
